53bf939fab9e938f211f3f081e546a53cb5078b5
abel7252 <abel7252@outlook.com>
2025-01-14 03:54:24 +0800

    rebuild with rust

--- build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf build
-mkdir build
-cd build
-
-cmake DCMAKE_TOOLCHAIN_FILE=../mingw-toolchain.cmake ..
-
-# 构建项目
-cmake --build .
--- build/CMakeFiles/pixel-vim.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pixel-vim.
-# This may be replaced when dependencies are built.
